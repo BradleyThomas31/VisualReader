7a8ab7a3bb81f3043375c3d9e66994653ad5a597
Bradley Thomas <bradleythomas@Bradleys-MacBook-Air.local>
2024-01-23 15:23:57 -0500

    created project

---
+++ VisualReader/VisualReader/VisualReaderApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualReaderApp.swift
+//  VisualReader
+//
+//  Created by Bradley Thomas on 1/23/24.
+//
+
+import SwiftUI
+
+@main
+struct VisualReaderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
